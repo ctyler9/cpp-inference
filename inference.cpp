298b77690a3f1a84c579a9eebf560f30fdb52ece
Clay Tyler <ctyler9@me.com>
2024-05-15 22:40:31 -0500

    update

--- inference.cpp
+++ inference.cpp
@@ -1,4 +1,11 @@
-#include "libs/onnxruntime/include/onnxruntime_cxx_api.h"
+
+// https://github.com/microsoft/onnxruntime/blob/v1.8.2/csharp/test/Microsoft.ML.OnnxRuntime.EndToEndTests.Capi/CXX_Api_Sample.cpp
+// https://github.com/microsoft/onnxruntime/blob/v1.8.2/include/onnxruntime/core/session/onnxruntime_cxx_api.h
+#include "./libs/onnxruntime/include/onnxruntime_cxx_api.h"
+
+#include <opencv2/dnn/dnn.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
 
 #include <chrono>
 #include <cmath>
@@ -10,13 +17,96 @@
 #include <string>
 #include <vector>
 
-#include <opencv2/dnn/dnn.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/opencv.hpp>
-
 template <typename T> T vectorProduct(const std::vector<T> &v) {
   return accumulate(v.begin(), v.end(), 1, std::multiplies<T>());
+}
+
+/**
+ * @brief Operator overloading for printing vectors
+ * @tparam T
+ * @param os
+ * @param v
+ * @return std::ostream&
+ */
+template <typename T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v) {
+  os << "[";
+  for (int i = 0; i < v.size(); ++i) {
+    os << v[i];
+    if (i != v.size() - 1) {
+      os << ", ";
+    }
+  }
+  os << "]";
+  return os;
+}
+
+/**
+ * @brief Print ONNX tensor data type
+ * https://github.com/microsoft/onnxruntime/blob/rel-1.6.0/include/onnxruntime/core/session/onnxruntime_c_api.h#L93
+ * @param os
+ * @param type
+ * @return std::ostream&
+ */
+std::ostream &operator<<(std::ostream &os,
+                         const ONNXTensorElementDataType &type) {
+  switch (type) {
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UNDEFINED:
+    os << "undefined";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT:
+    os << "float";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT8:
+    os << "uint8_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT8:
+    os << "int8_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT16:
+    os << "uint16_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT16:
+    os << "int16_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT32:
+    os << "int32_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_INT64:
+    os << "int64_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_STRING:
+    os << "std::string";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BOOL:
+    os << "bool";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_FLOAT16:
+    os << "float16";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_DOUBLE:
+    os << "double";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT32:
+    os << "uint32_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_UINT64:
+    os << "uint64_t";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX64:
+    os << "float real + float imaginary";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_COMPLEX128:
+    os << "double real + float imaginary";
+    break;
+  case ONNXTensorElementDataType::ONNX_TENSOR_ELEMENT_DATA_TYPE_BFLOAT16:
+    os << "bfloat16";
+    break;
+  default:
+    break;
+  }
+
+  return os;
 }
 
 std::vector<std::string> readLabels(std::string &labelFilepath) {
@@ -29,30 +119,59 @@ std::vector<std::string> readLabels(std::string &labelFilepath) {
   return labels;
 }
 
-int main() {
+int main(int argc, char *argv[]) {
   const int64_t batchSize = 2;
-  bool useCUDA{false};
+  bool useCUDA{true};
+  const char *useCUDAFlag = "--use_cuda";
+  const char *useCPUFlag = "--use_cpu";
+  if (argc == 1) {
+    useCUDA = false;
+  } else if ((argc == 2) && (strcmp(argv[1], useCUDAFlag) == 0)) {
+    useCUDA = true;
+  } else if ((argc == 2) && (strcmp(argv[1], useCPUFlag) == 0)) {
+    useCUDA = false;
+  } else if ((argc == 2) && (strcmp(argv[1], useCUDAFlag) != 0)) {
+    useCUDA = false;
+  } else {
+    throw std::runtime_error{"Too many arguments."};
+  }
+
+  if (useCUDA) {
+    std::cout << "Inference Execution Provider: CUDA" << std::endl;
+  } else {
+    std::cout << "Inference Execution Provider: CPU" << std::endl;
+  }
 
   std::string instanceName{"image-classification-inference"};
-  std::string modelFilePath{"./data/resnet18-v1-7.onnx"};
-  std::string imageFilePath{"./data/birb.jpg"};
-  std::string labelFilePath{"./data/synset.txt"};
+  // std::string modelFilepath{"../../data/models/squeezenet1.1-7.onnx"};
+  std::string modelFilepath{"./data/resnet18-v1-7.onnx"};
+  std::string imageFilepath{"./data/birb.jpg"};
+  std::string labelFilepath{"./data/synset.txt"};
 
-  std::vector<std::string> labels{readLabels(labelFilePath)};
+  std::vector<std::string> labels{readLabels(labelFilepath)};
 
   Ort::Env env(OrtLoggingLevel::ORT_LOGGING_LEVEL_WARNING,
                instanceName.c_str());
   Ort::SessionOptions sessionOptions;
-  sessionOptions.SetInterOpNumThreads(1);
+  sessionOptions.SetIntraOpNumThreads(1);
   if (useCUDA) {
+    // Using CUDA backend
+    // https://github.com/microsoft/onnxruntime/blob/v1.8.2/include/onnxruntime/core/session/onnxruntime_cxx_api.h#L329
     OrtCUDAProviderOptions cuda_options{};
     sessionOptions.AppendExecutionProvider_CUDA(cuda_options);
   }
 
+  // Sets graph optimization level
+  // Available levels are
+  // ORT_DISABLE_ALL -> To disable all optimizations
+  // ORT_ENABLE_BASIC -> To enable basic optimizations (Such as redundant node
+  // removals) ORT_ENABLE_EXTENDED -> To enable extended optimizations
+  // (Includes level 1 + more complex optimizations like node fusions)
+  // ORT_ENABLE_ALL -> To Enable All possible optimizations
   sessionOptions.SetGraphOptimizationLevel(
       GraphOptimizationLevel::ORT_ENABLE_EXTENDED);
 
-  Ort::Session session(env, modelFilePath.c_str(), sessionOptions);
+  Ort::Session session(env, modelFilepath.c_str(), sessionOptions);
 
   Ort::AllocatorWithDefaultOptions allocator;
 
@@ -91,12 +210,12 @@ int main() {
   std::cout << "Number of Output Nodes: " << numOutputNodes << std::endl;
   std::cout << "Input Name: " << inputName << std::endl;
   std::cout << "Input Type: " << inputType << std::endl;
-  // std::cout << "Input Dimensions: " << inputDims << std::endl;
+  std::cout << "Input Dimensions: " << inputDims << std::endl;
   std::cout << "Output Name: " << outputName << std::endl;
   std::cout << "Output Type: " << outputType << std::endl;
-  // std::cout << "Output Dimensions: " << outputDims << std::endl;
+  std::cout << "Output Dimensions: " << outputDims << std::endl;
 
-  cv::Mat imageBGR = cv::imread(imageFilePath, cv::ImreadModes::IMREAD_COLOR);
+  cv::Mat imageBGR = cv::imread(imageFilepath, cv::ImreadModes::IMREAD_COLOR);
   cv::Mat resizedImageBGR, resizedImageRGB, resizedImage, preprocessedImage;
   cv::resize(imageBGR, resizedImageBGR,
              cv::Size(inputDims.at(3), inputDims.at(2)),
@@ -107,16 +226,19 @@ int main() {
 
   cv::Mat channels[3];
   cv::split(resizedImage, channels);
-
+  // Normalization per channel
+  // Normalization parameters obtained from
+  // https://github.com/onnx/models/tree/master/vision/classification/squeezenet
   channels[0] = (channels[0] - 0.485) / 0.229;
   channels[1] = (channels[1] - 0.456) / 0.224;
   channels[2] = (channels[2] - 0.406) / 0.225;
   cv::merge(channels, 3, resizedImage);
+  // HWC to CHW
   cv::dnn::blobFromImage(resizedImage, preprocessedImage);
 
   size_t inputTensorSize = vectorProduct(inputDims);
   std::vector<float> inputTensorValues(inputTensorSize);
-
+  // Make copies of the same image input.
   for (int64_t i = 0; i < batchSize; ++i) {
     std::copy(preprocessedImage.begin<float>(), preprocessedImage.end<float>(),
               inputTensorValues.begin() + i * inputTensorSize / batchSize);
@@ -134,7 +256,6 @@ int main() {
 
   Ort::MemoryInfo memoryInfo = Ort::MemoryInfo::CreateCpu(
       OrtAllocatorType::OrtArenaAllocator, OrtMemType::OrtMemTypeDefault);
-
   inputTensors.push_back(Ort::Value::CreateTensor<float>(
       memoryInfo, inputTensorValues.data(), inputTensorSize, inputDims.data(),
       inputDims.size()));
@@ -191,6 +312,4 @@ int main() {
                        .count() /
                    static_cast<float>(numTests)
             << " ms" << std::endl;
-
-  return 0;
 }
